a46ac48891ab4fd2e25839c3bde651bc7dd8ec26
ArtichautDev <91349238+ArtichautLearnCode@users.noreply.github.com>
2022-08-30 16:13:48 +0200

    Update start.sh

--- system-DO-NOT-DELETE/nginx/start.sh
+++ system-DO-NOT-DELETE/nginx/start.sh
@@ -1,9 +1,8 @@
 #!/bin/ash
-
+echo "Web Server: http://45.140.164.98:port"
+echo ""
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm7 --fpm-config /home/container/system-DO-NOT-DELETE/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
 /usr/sbin/nginx -c /home/container/system-DO-NOT-DELETE/nginx/nginx.conf
-
-echo "Web Server: http://45.140.164.98:port"
